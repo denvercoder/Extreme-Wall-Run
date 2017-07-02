fef2be41ba68a011683f3bc18d03fc69b16ea967
Tim Myers <realdenvercoder@gmail.com>
2017-07-02 05:28:22 -0600

    implement admob

--- DontFall/GameViewController.swift
+++ DontFall/GameViewController.swift
@@ -12,13 +12,15 @@ import QuartzCore
 import SceneKit
 import SpriteKit
 import GameKit
+import GoogleMobileAds
+import AudioToolbox
 
 struct bodyNames {
     static let Person = 0x1 << 1
     static let Coin = 0x1 << 2
 }
 
-class GameViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsContactDelegate, GKGameCenterControllerDelegate{
+class GameViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysicsContactDelegate, GKGameCenterControllerDelegate, GADBannerViewDelegate{
     let scene = SCNScene()
     let cameraNode = SCNNode()
     var person = SCNNode()
@@ -35,8 +37,13 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysics
     var highscoreLabel = UILabel()
     var gameButton = UIButton()
     
+    var adMobBannerView = GADBannerView()
+    let ADMOB_BANNER_UNIT_ID = "ca-app-pub-8033231278137446/3194184413"
+    
     override func viewDidLoad() {
         self.createScene()
+        
+        initAdMobBanner()
         
         authenticatePlayer()
         
@@ -64,6 +71,7 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysics
         gameButton.setImage(UIImage(named: "gamecenter"), for: .normal)
         gameButton.addTarget(self, action: #selector(GameViewController.showLeaderboard), for: UIControlEvents.touchUpInside)
         self.view.addSubview(gameButton)
+        
         
         
         
@@ -380,4 +388,55 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate, SCNPhysics
     func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
         gameCenterViewController.dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: -  ADMOB BANNER
+    func initAdMobBanner() {
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            // iPhone
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 320, height: 50))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 320, height: 50)
+        } else  {
+            // iPad
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 468, height: 60))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 468, height: 60)
+        }
+        
+        adMobBannerView.adUnitID = ADMOB_BANNER_UNIT_ID
+        adMobBannerView.rootViewController = self
+        adMobBannerView.delegate = self
+        view.addSubview(adMobBannerView)
+        
+        let request = GADRequest()
+        adMobBannerView.load(request)
+    }
+    
+    
+    // Hide the banner
+    func hideBanner(_ banner: UIView) {
+        UIView.beginAnimations("hideBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = true
+    }
+    
+    // Show the banner
+    func showBanner(_ banner: UIView) {
+        UIView.beginAnimations("showBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = false
+    }
+    
+    // AdMob banner available
+    func adViewDidReceiveAd(_ view: GADBannerView) {
+        showBanner(adMobBannerView)
+    }
+    
+    // NO AdMob banner available
+    func adView(_ view: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        hideBanner(adMobBannerView)
+    }
+
+
 } //end of class
